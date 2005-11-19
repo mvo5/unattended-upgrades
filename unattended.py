440a14793e7080887d50f1808573cf88af5b5f9c
Michael Vogt <egon@sam>
2005-11-19 13:15:30 +0100

    * conffile prompt detection added

--- unattended.py
+++ unattended.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.4
 
-import apt, apt_pkg
-import sys, os
+import apt, apt_pkg, apt_inst
+import sys, os, string
 from optparse import OptionParser
 
 class MyCache(apt.Cache):
@@ -36,6 +36,32 @@ def check_changes_for_sanity(cache, allowed_origins):
                 return False
     return True
 
+def pkgname_from_deb(debfile):
+    # FIXME: add error checking here
+    control = apt_inst.debExtractControl(open(debfile))
+    sections = apt_pkg.ParseSection(control)
+    return sections["Package"]
+
+def conffile_prompt(destFile):
+    print "check_conffile_prompt('%s')" % destFile
+    pkgname = pkgname_from_deb(destFile)
+    status_file = apt_pkg.Config.Find("Dir::State::status")
+    parse = apt_pkg.ParseTagFile(open(status_file,"r"))
+    while parse.Step() == 1:
+        if parse.Section.get("Package") == pkgname:
+            print "found pkg: %s" % pkgname
+            if parse.Section.has_key("Conffiles"):
+                conffiles = parse.Section.get("Conffiles")
+                # Conffiles:
+                # /etc/bash_completion.d/m-a c7780fab6b14d75ca54e11e992a6c11c
+                for line in string.split(conffiles,"\n"):
+                    (file, md5) = string.split(string.strip(line))
+                    if os.path.exists(file):
+                        current_md5 = apt_pkg.md5sum(open(file).read())
+                        if current_md5 != md5:
+                            return True
+    return False
+
 
 if __name__ == "__main__":
 
@@ -55,6 +81,14 @@ if __name__ == "__main__":
         except OSError:
             pass
         apt_pkg.Config.Set("Dir::Cache::archives",dir)
+
+        # FIXME: add tests for the conffile_prompt code
+        #if conffile_prompt(sys.argv[1]):
+        #    print "will prompt"
+        #else:
+        #    print "wont prompt"
+        #sys.exit(1)
+
 
     # get a cache
     cache = MyCache()
@@ -85,16 +119,19 @@ if __name__ == "__main__":
     list = apt_pkg.GetPkgSourceList()
     list.ReadMainList()
     recs = apt_pkg.GetPkgRecords(cache._cache)
-    fetcher = apt_pkg.GetAcquire()
+    if debug:
+        fetcher = apt_pkg.GetAcquire(apt.progress.TextFetchProgress())
+    else:
+        fetcher = apt_pkg.GetAcquire()
     pm = apt_pkg.GetPackageManager(cache._depcache)
     pm.GetArchives(fetcher,list,recs)
-    for item in fetcher.Items:
-        #print "%s -> %s:\n Status: %s Complete: %s IsTrusted: %s" % \
-        #      (item.DescURI, item.DestFile,  item.Status,
-        #       item.Complete,  item.IsTrusted)
-        if item.Status == item.StatError:
-            print "Some error ocured: '%s'" % item.ErrorText
-        print
+    #for item in fetcher.Items:
+    #    #print "%s -> %s:\n Status: %s Complete: %s IsTrusted: %s" % \
+    #    #      (item.DescURI, item.DestFile,  item.Status,
+    #    #       item.Complete,  item.IsTrusted)
+    #    if item.Status == item.StatError:
+    #        print "Some error ocured: '%s'" % item.ErrorText
+    #    #print
     res = fetcher.Run()
 
     # now check the downloaded debs for conffile conflicts
@@ -106,5 +143,10 @@ if __name__ == "__main__":
             print "Some error ocured: '%s'" % item.ErrorText
         if item.Complete == False:
             print "No error, still nothing downloaded"
-        print
-        # check_conffile_prompt(item.DestFile) 
+            continue
+        if conffile_prompt(item.DestFile):
+            # FIXME: skip package (means to re-run the whole marking again
+            # and making sure that the package will not be pulled in by
+            # some other package again!
+            print "pkg '%s' has conffile prompt" % pkgname_from_deb(item.DestFile)
+            
