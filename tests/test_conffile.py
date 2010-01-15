fe6c62e7b6dbbec95e073ee488c9187d89b53412
Michael Vogt <michael.vogt@ubuntu.com>
2010-01-15 11:47:19 +0100

    add postivie test for conffile prompt

--- tests/test_conffile.py
+++ tests/test_conffile.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 
+import apt_pkg
 import logging
+import unittest
 import sys
+
 from unattended_upgrade import conffile_prompt
+
+class TestConffilePrompt(unittest.TestCase):
+    def setUp(self):
+        apt_pkg.Config.Set("Dir::State::status", "./var/lib/dpkg/status")
+
+    def testWillPrompt(self):
+        test_pkg = "./packages/conf-test-package_1.1.deb"
+        self.assertTrue(conffile_prompt(test_pkg), 
+                        "conffile prompt detection incorrect")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    test_pkg = sys.argv[1]
-    has_conffile_prompt = conffile_prompt(test_pkg)
-    logging.debug("will have conf file prompt: %s" % has_conffile_prompt)
+    #logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
+
