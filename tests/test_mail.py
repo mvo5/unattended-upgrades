2ef39a9c62b2e1a58c8e8345a33db7ccadefb1cf
Michael Vogt <michael.vogt@ubuntu.com>
2010-05-17 12:31:35 +0200

    unattended-upgrades: add test for reboot required mail

---
+++ tests/test_mail.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import apt
+import apt_pkg
+import os
+import logging
+import unittest
+import sys
+
+from StringIO import StringIO
+
+import unattended_upgrade
+from unattended_upgrade import send_summary_mail
+
+class TestSendSummaryMail(unittest.TestCase):
+
+    def setUp(self):
+        # monkey patch to make it testable
+        unattended_upgrade.REBOOT_REQUIRED_FILE = "./reboot-required"
+        # mock-mail binary that creates a mail.txt file
+        unattended_upgrade.MAIL_BINARY = "./mock-mail"
+
+    def _return_mock_data(self):
+        pkgs = "\n".join(["2vcard"])
+        res = True
+        pkgs_kept_back = []
+        mem_log = StringIO("mem_log text")
+        logfile_dpkg = "./apt-term.log"
+        open("./apt-term.log", "w").write("logfile_dpkg text")
+        return (pkgs, res, pkgs_kept_back, mem_log, logfile_dpkg)
+
+    def testSummaryMailReboot(self):
+        # setup mail
+        apt_pkg.Config.set("Unattended-Upgrade::Mail", "root")
+        open("./reboot-required","w").write("")
+        send_summary_mail(*self._return_mock_data())
+        self.assertTrue("[reboot required]" in open("mail.txt").read())
+
+    def testSummaryMailNoReboot(self):
+        # setup mail
+        apt_pkg.Config.set("Unattended-Upgrade::Mail", "root")
+        if os.path.exists("./reboot-required"):
+            os.unlink("./reboot-required")
+        send_summary_mail(*self._return_mock_data())
+        self.assertFalse("[reboot required]" in open("mail.txt").read())
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
+
