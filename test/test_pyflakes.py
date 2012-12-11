0022289c85ad3ceafc61f90867f1678a3fb8646a
Michael Vogt <michael.vogt@ubuntu.com>
2012-12-11 18:52:25 +0100

    * tests/*.py:   - updated pep8 tests, make pyflakes test "test" dir too

--- test/test_pyflakes.py
+++ test/test_pyflakes.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import subprocess
 import unittest
@@ -7,9 +8,11 @@ class TestPyflakesClean(unittest.TestCase):
     """ ensure that the tree is pyflakes clean """
 
     def test_pyflakes_clean(self):
-        path = os.path.join(
-            os.path.dirname(__file__), "unattended_upgrade.py")
-        self.assertEqual(subprocess.call(["pyflakes",  path]), 0)
+        files = glob.glob(
+            os.path.join(os.path.dirname(__file__), "..", "test", "*.py"))
+        files.append(
+            os.path.join(os.path.dirname(__file__), "unattended_upgrade.py"))
+        self.assertEqual(subprocess.call(["pyflakes"] + files), 0)
 
 
 if __name__ == "__main__":
--- test/test_regression.py
+++ test/test_regression.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import io
-import logging
 import os
 import tempfile
 import unittest
@@ -10,7 +9,7 @@ import unittest
 from mock import (
     Mock,
     patch,
-    )
+)
 
 from unattended_upgrade import do_install
 
