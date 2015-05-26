233de9518b018bf1ee2f8e212d7fd709813ef86c
Michael Vogt <mvo@ubuntu.com>
2015-05-26 08:51:04 +0200

    Refactor reboot_if_requested_and_needed()
    Extract method "logged_in_users()", simplify code a bit, add more
    tests, use ngettext()
    
    Git-dch: ignore

--- test/test_reboot.py
+++ test/test_reboot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
-import apt_pkg
+import datetime
 import logging
-import unittest
 import os
+import unittest
+
+import apt_pkg
 
 from mock import (
     patch,
@@ -26,6 +28,18 @@ class RebootTestCase(unittest.TestCase):
         apt_pkg.config.set("Unattended-Upgrade::Automatic-Reboot-WithUsers", "1")
 
     @patch("subprocess.call")
+    def test_no_reboot_done_because_no_stamp(self, mock_call):
+        unattended_upgrade.REBOOT_REQUIRED_FILE = "/no/such/file/or/directory"
+        unattended_upgrade.reboot_if_requested_and_needed()
+        self.assertEqual(mock_call.called, False)
+
+    @patch("subprocess.call")
+    def test_no_reboot_done_because_no_option(self, mock_call):
+        apt_pkg.config.set("Unattended-Upgrade::Automatic-Reboot", "0")
+        unattended_upgrade.reboot_if_requested_and_needed()
+        self.assertEqual(mock_call.called, False)
+        
+    @patch("subprocess.call")
     def test_reboot_now(self, mock_call):
         unattended_upgrade.reboot_if_requested_and_needed()
         mock_call.assert_called_with(["/sbin/shutdown", "-r", "now"])
@@ -39,6 +53,7 @@ class RebootTestCase(unittest.TestCase):
 
     @patch("subprocess.call")
     def test_reboot_withoutusers(self, mock_call):
+        """Ensure that a reboot happens when no users are logged in"""
         apt_pkg.config.set(
             "Unattended-Upgrade::Automatic-Reboot-WithUsers", "0")
         apt_pkg.config.set(
@@ -50,12 +65,26 @@ class RebootTestCase(unittest.TestCase):
 
     @patch("subprocess.call")
     def test_reboot_withusers(self, mock_call):
+        """Ensure that a reboot does not happen if a user is logged in"""
         apt_pkg.config.set(
             "Unattended-Upgrade::Automatic-Reboot-WithUsers", "0")
         # some pgm that allways output a word
         unattended_upgrade.USERS = "/bin/uname"
         unattended_upgrade.reboot_if_requested_and_needed()
-        assert not mock_call.called, "couldn't detect active sessions"
+        self.assertEqual(
+            mock_call.called, False,
+            "Called '%s' when nothing should have "
+            "happen" % mock_call.call_args_list)
+
+    @patch("subprocess.call")
+    def test_logged_in_users(self, mock_call):
+        # some pgm that allways output a word
+        unattended_upgrade.USERS = ["/bin/date", "+%Y %m"]
+        users = unattended_upgrade.logged_in_users()
+        today = datetime.date.today()
+        self.assertEqual(users, [
+            today.strftime("%Y"), today.strftime("%m")])
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
