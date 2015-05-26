923cde4525b6b108f633e10d7226d19e1598f977
Michael Vogt <mvo@ubuntu.com>
2015-05-26 13:50:33 +0200

    test/test_regression.py: skip test under py2

--- test/test_regression.py
+++ test/test_regression.py
@@ -3,6 +3,7 @@
 
 import io
 import os
+import sys
 import tempfile
 import unittest
 
@@ -14,8 +15,16 @@ from mock import (
 from unattended_upgrade import do_install
 
 
+class MockCache:
+    def __iter__(self):
+        raise StopIteration
+    def get_changes(self):
+        return []
+
+
 class TestRegression(unittest.TestCase):
 
+    @unittest.skipIf(sys.version_info[0] != 3, "only works on py3")
     @patch("unattended_upgrade.upgrade_normal")
     def test_do_install_fail_unicode_write(self, mock_upgrade_normal):
         """ test if the substitute function works """
@@ -23,6 +32,7 @@ class TestRegression(unittest.TestCase):
             raise Exception("meepä")
         tmp = tempfile.TemporaryFile()
         old_stderr = os.dup(2)
+        # comment this line if you need to debug stuff
         os.dup2(tmp.fileno(), 2)
 
         mock_upgrade_normal.side_effect = _raise
@@ -33,14 +43,19 @@ class TestRegression(unittest.TestCase):
         # mock options
         options = Mock()
         options.minimal_upgrade_steps = False
-        do_install(cache=Mock(), pkgs_to_upgrade=[pkg], blacklisted_pkgs=[],
-                   options=options, logfile_dpkg=logfile_dpkg)
+        do_install(cache=MockCache(), pkgs_to_upgrade=[pkg],
+                   blacklisted_pkgs=[],
+                   whitelisted_pkgs=[], options=options,
+                   logfile_dpkg=logfile_dpkg)
         # if there is no exception here, we are good
         os.dup2(old_stderr, 2)
         tmp.seek(0)
-        self.assertEqual(tmp.read().decode("utf-8"), "Exception: meepä\n")
+        content = tmp.read().decode("utf-8").splitlines()
+        tmp.close()
+        self.assertEqual(content[-1], "Exception: meepä")
 
 
 if __name__ == "__main__":
-    #logging.basicConfig(level=logging.DEBUG)
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
     unittest.main()
