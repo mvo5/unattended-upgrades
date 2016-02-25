0e0299e0fb0d9193f1b789f0732f61e6726d2def
Michael Vogt <mvo@ubuntu.com>
2016-02-25 10:42:35 +0100

    pep8 and small logging fixes

--- test/test_patch_days.py
+++ test/test_patch_days.py
@@ -8,7 +8,11 @@ import apt
 
 import unattended_upgrade
 
+
 class TestUpdateDays(unittest.TestCase):
+
+    def setUp(self):
+        apt.apt_pkg.config.clear("Unattended-Upgrade::Update-Days")
 
     def test_update_days_no_patch_days_always_runs_uu(self):
         apt.apt_pkg.config.clear("Unattended-Upgrade::Update-Days")
@@ -44,7 +48,6 @@ class TestUpdateDays(unittest.TestCase):
         # that was a Sun
         mock_date.today.return_value = date(2007, 7, 29)
         self.assertTrue(unattended_upgrade.is_update_day())
-
 
 
 if __name__ == "__main__":
