229f21061ff7d2d94858174452f18b0d038f79af
Balint Reczey <balint.reczey@canonical.com>
2018-05-07 17:00:02 +0200

    End Python 2 support
    Fixes #113

--- test/test_pyflakes.py
+++ test/test_pyflakes.py
@@ -13,7 +13,7 @@ class TestPyflakesClean(unittest.TestCase):
             os.path.join(top_src_dir, "unattended-upgrade"),
             os.path.join(top_src_dir, "unattended-upgrade-shutdown"),
         ]
-        self.assertEqual(subprocess.call(["pyflakes", ] + targets), 0)
+        self.assertEqual(subprocess.call(["pyflakes3", ] + targets), 0)
 
 
 if __name__ == "__main__":
