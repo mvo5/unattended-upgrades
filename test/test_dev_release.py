8fa7186908920902a675b1901bf7bbec635f4929
Julian Andres Klode <julian.klode@canonical.com>
2018-11-27 11:50:43 +0100

    test_dev_release: Test Unattended-Upgrade::DevRelease=auto
    We need to do some magic with the root logging handlers, as otherwise
    logging does not get setup again, and we end up with no log file at
    all.

--- test/test_dev_release.py
+++ test/test_dev_release.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import datetime
+import logging
 import os
 import unittest
 
@@ -16,6 +18,46 @@ class MockOptions(object):
     dry_run = True
     apt_debug = False
     minimal_upgrade_steps = False
+
+
+class MockDistroAuto(object):
+
+    start_updates = datetime.date.today()
+    release = (datetime.date.today()
+               + unattended_upgrade.DEVEL_UNTIL_RELEASE
+               + datetime.timedelta(days=1))
+    series = "artful"
+
+    def devel(self, result):
+        assert result == "object"
+        return self
+
+
+class MockDistroNoAuto(object):
+
+    release = (datetime.date.today()
+               + unattended_upgrade.DEVEL_UNTIL_RELEASE)
+    series = "artful"
+
+    def devel(self, result):
+        assert result == "object"
+        return self
+
+
+class MockDistroNoRelease(object):
+
+    release = None
+    series = "artful"
+
+    def devel(self, result):
+        assert result == "object"
+        return self
+
+
+class MockDistroInfoModule(object):
+
+    def __init__(self, ubuntu):
+        self.UbuntuDistroInfo = ubuntu
 
 
 class TestUntrusted(unittest.TestCase):
@@ -36,6 +78,9 @@ class TestUntrusted(unittest.TestCase):
                                      "apt.conf")
 
         os.rename(self.apt_conf, self.apt_conf + ".bak")
+        for hdlr in logging.root.handlers:
+            hdlr.close()
+        logging.root.handlers = []
 
     def tearDown(self):
         os.remove(self.log)
@@ -57,7 +102,67 @@ Unattended-Upgrade::OnlyOnAcPower "false";
         unattended_upgrade.main(options, rootdir=self.rootdir)
         # read the log to see what happend
         with open(self.log) as f:
-            needle = "Not running on the development release."
+            needle = "Not running on the development release"
+            haystack = f.read()
+            self.assertTrue(needle in haystack,
+                            "Can not find '%s' in '%s'" % (needle, haystack))
+
+    def test_auto_devrelease(self):
+        """We are not ready to auto upgrade just yet"""
+        self.write_config("auto")
+
+        # run it
+        unattended_upgrade.distro_info = MockDistroInfoModule(MockDistroAuto)
+
+        options = MockOptions()
+        unattended_upgrade.DISTRO_DESC = "Artful Aardvark (development branch)"
+        unattended_upgrade.DISTRO_CODENAME = "artful"
+        unattended_upgrade.DISTRO_ID = "ubuntu"
+        unattended_upgrade.main(options, rootdir=self.rootdir)
+        # read the log to see what happend
+        with open(self.log) as f:
+            needle = ("Not running on this development release before %s"
+                      % MockDistroAuto.start_updates)
+            haystack = f.read()
+            self.assertTrue(needle in haystack,
+                            "Can not find '%s' in '%s'" % (needle, haystack))
+
+    def test_noauto_devrelease(self):
+        """We are in the valid time period"""
+        self.write_config("auto")
+        # run it
+        unattended_upgrade.distro_info = MockDistroInfoModule(MockDistroNoAuto)
+
+        options = MockOptions()
+        unattended_upgrade.DISTRO_DESC = "Artful Aardvark (development branch)"
+        unattended_upgrade.DISTRO_CODENAME = "artful"
+        unattended_upgrade.DISTRO_ID = "ubuntu"
+        unattended_upgrade.main(options, rootdir=self.rootdir)
+        # read the log to see what happend
+        with open(self.log) as f:
+            # Check that we could have run
+            needle = ("Lock could not be acquired "
+                      "(another package manager running?)")
+            haystack = f.read()
+            self.assertTrue(needle in haystack,
+                            "Can not find '%s' in '%s'" % (needle, haystack))
+
+    def test_norelease_devrelease(self):
+        """The devel series has no release update, so do updates"""
+        self.write_config("auto")
+        # run it
+        unattended_upgrade.distro_info = MockDistroInfoModule(MockDistroNoAuto)
+
+        options = MockOptions()
+        unattended_upgrade.DISTRO_DESC = "Artful Aardvark (development branch)"
+        unattended_upgrade.DISTRO_CODENAME = "artful"
+        unattended_upgrade.DISTRO_ID = "ubuntu"
+        unattended_upgrade.main(options, rootdir=self.rootdir)
+        # read the log to see what happend
+        with open(self.log) as f:
+            # Check that we could have run
+            needle = ("Lock could not be acquired "
+                      "(another package manager running?)")
             haystack = f.read()
             self.assertTrue(needle in haystack,
                             "Can not find '%s' in '%s'" % (needle, haystack))
