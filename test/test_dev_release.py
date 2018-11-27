91d4a465909461cd9009fb871fe9f569471a10be
Julian Andres Klode <julian.klode@canonical.com>
2018-11-27 11:11:59 +0100

    test_dev_release: Fix and enable test
    Enable the test, this requires overriding the LOCK_FILE to
    not use the global one. Also set some more config options
    needed for travis, and abstract the config writing into a
    method so we don't repeat ourselves if we add more test
    cases.
    
    Since we are sharing root.untrusted with another test,
    we also have to make sure not to replace it's apt.conf,
    so we just move it out of the way in setUp and back in
    tearDown.

--- test/test_dev_release.py
+++ test/test_dev_release.py
@@ -21,19 +21,35 @@ class MockOptions(object):
 class TestUntrusted(unittest.TestCase):
 
     def setUp(self):
+        apt.apt_pkg.config.set("Unattended-Upgrade::"
+                               "Skip-Updates-On-Metered-Connections",
+                               "false")
+        apt.apt_pkg.config.set("Unattended-Upgrade::OnlyOnAcPower",
+                               "false")
+        unattended_upgrade.LOCK_FILE = "./u-u.lock"
         self.rootdir = os.path.abspath("./root.untrusted")
         self.log = os.path.join(
             self.rootdir, "var", "log", "unattended-upgrades",
             "unattended-upgrades.log")
 
+        self.apt_conf = os.path.join(self.rootdir, "etc", "apt",
+                                     "apt.conf")
+
+        os.rename(self.apt_conf, self.apt_conf + ".bak")
+
     def tearDown(self):
         os.remove(self.log)
+        os.rename(self.apt_conf + ".bak", self.apt_conf)
+
+    def write_config(self, devrelease):
+        with open(self.apt_conf, "w") as fp:
+            fp.write("""Unattended-Upgrade::DevRelease "%s";
+Unattended-Upgrade::Skip-Updates-On-Metered-Connections "false";
+Unattended-Upgrade::OnlyOnAcPower "false";
+""" % devrelease)
 
     def test_do_not_run_on_devrelease(self):
-        apt_conf = os.path.join(self.rootdir, "etc", "apt", "apt.conf")
-        with open(apt_conf, "w") as fp:
-            fp.write("""Unattended-Upgrade::DevRelease "false";
-""")
+        self.write_config("false")
 
         # run it
         options = MockOptions()
