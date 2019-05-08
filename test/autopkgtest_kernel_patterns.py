567d58671bb15193cd8ef429b9cdf0b11f11f3da
Balint Reczey <balint.reczey@canonical.com>
2019-05-08 20:33:15 +0200

    Packages including kernel version without flavor in their name may be kernel packages
    Also add autopkgtest for checking if the generated patterns cover the currently running kernel.
    
    LP: #1828200

---
+++ test/autopkgtest_kernel_patterns.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import apt_pkg
+import logging
+import re
+import subprocess
+import unittest
+
+from unattended_upgrade import (running_kernel_pkgs_regexp,
+                                versioned_kernel_pkgs_regexp)
+
+
+class TestKernelPatterns(unittest.TestCase):
+
+    def test_versioned(self):
+        """kernel package patterns should cover versioned packages"""
+        versioned_regexp = versioned_kernel_pkgs_regexp()
+        running_regexp = running_kernel_pkgs_regexp()
+        running_kernel_version = subprocess.check_output(
+            ["uname", "-r"], universal_newlines=True).rstrip()
+        running_escaped_regexp = ".*" + re.escape(running_kernel_version)
+        try:
+            running_noflavor_regexp = ".*" + re.escape(
+                re.match("[1-9][0-9]*\\.[0-9]+\\.[0-9]+-[0-9]+",
+                         running_kernel_version)[0])
+        except TypeError:
+            self.skipTest("Could not find flavor of running kernel. It may "
+                          "not be a packaged one.")
+
+        cache = apt_pkg.Cache(None)
+        not_matched = set()
+        for pkg in cache.packages:
+            pkg_name = pkg.name
+            if re.match(running_noflavor_regexp, pkg_name):
+                if re.match(running_escaped_regexp, pkg_name) \
+                   and not re.match(running_regexp, pkg_name):
+                    not_matched.add(pkg_name)
+                    continue
+                if not re.match(versioned_regexp, pkg_name):
+                    not_matched.add(pkg.name)
+
+        self.assertTrue(not not_matched,
+                        "kernel packages not matched: %s"
+                        % " ".join(not_matched))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
