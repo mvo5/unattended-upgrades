c849f76b328b55d199f1a81f74fd442748b2d77e
Balint Reczey <balint.reczey@canonical.com>
2019-05-21 11:54:42 +0200

    Store list of kept packages and report the number of them in motd
    LP: #1823070

---
+++ test/test_motd.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import shutil
+import tempfile
+import unittest
+
+from unattended_upgrade import update_kept_packages
+
+
+class MotdTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.tmpdir)
+        self.addCleanup(logging.shutdown)
+
+    def test_packages_kept(self):
+        pkgs_kept_back = {"Debian wheezy-security": ["linux-image"],
+                          "Debian wheezy": ["hello", "tworld"]}
+        update_kept_packages(pkgs_kept_back, os.path.join(self.tmpdir,
+                                                          "kept-back"))
+        with open(os.path.join(self.tmpdir, "kept-back"), "rb") as fp:
+            kept_txt = fp.read().decode("utf-8")
+        self.assertEqual('hello linux-image tworld', kept_txt)
+        update_kept_packages({}, os.path.join(self.tmpdir, "kept-back"))
+        self.assertFalse(
+            os.path.exists(os.path.join(self.tmpdir, "kept-back")))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
