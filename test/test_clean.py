fe5f4f90d4e00f26a170570633ab33e59ff17b1c
Michael Vogt <mvo@ubuntu.com>
2022-02-23 19:33:41 +0100

    tests: make "test_clean.py" work outside cwd

--- test/test_clean.py
+++ test/test_clean.py
@@ -2,12 +2,13 @@
 
 
 import os
+import os.path
 import shutil
 import tempfile
 import unittest
 
 import apt_pkg
-apt_pkg.config.set("Dir", "./aptroot")
+apt_pkg.config.set("Dir", os.path.join(os.path.dirname(__file__), "./aptroot"))
 import apt
 import unattended_upgrade
 
